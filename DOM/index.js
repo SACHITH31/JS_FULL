9a59afb0072ddf2e21938eb725a02e9825d3db30
KASA <kasasachith@gmail.com>
2025-11-16 11:09:44 +0530

    written ssample code

---
+++ DOM/index.js
@@ -0,0 +1,10 @@
+let temp = document.querySelector(".p-class-tag")
+console.log(temp)
+
+// temp.addEventListener('click', () => {
+//     console.log("Clicked On me")
+// })
+
+// for (let i = 0; i < temp.length; i++) {
+//     console.log(temp[i].textContent)
+// }
